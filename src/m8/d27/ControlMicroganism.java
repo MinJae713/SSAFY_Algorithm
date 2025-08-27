1443766d09a7e67abbcef56381068bd55c4be79b
YooMinJae <dbalsend@naver.com>
2025-08-28 00:48:04 +0900

    미생물 미완

--- src/m8/d27/ControlMicroganism.java
+++ src/m8/d27/ControlMicroganism.java
@@ -5,22 +5,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 /**
  * [문제 풀이]
  * 1. 1시간~격리 시간(checkTime) 동안 반복
  * 2. 미생물 그룹들(groups) 이동
- * 	2.1. 이동 후 이동 위치에 그룹 번호 추가
+ * 	2.1. 이동 후 위치에 그룹 번호가 없다면?
+ * 		2.1.1. 이동 위치에 그룹 번호 지정
+ * 	2.2. 그룹 번호가 있다면?
+ * 		2.2.1. 기존에 그 위치에 있던 그룹의 미생물 수, 이동하는 그룹의 미생물 수 비교
+ * 		2.2.2. 수가 많은 미생물 그룹에 적은 미생물 그룹의 미생물 수 합산
+ * 		2.2.3. 적은 수의 미생물 그룹은 그룹 삭제 대상에 추가
+ * 		2.2.4. 이동하는 그룹이 기존 그룹보다 큰 경우, 해당 이동 위치에 이동 그룹 번호 지정
  * 3. 각 미생물 그룹의 이동 결과 처리
- * 	3.1. 미생물 그룹이 영역 마지막 위치에 있다면?
- * 		3.1.1. 미생물 수 감소 및 방향 변경
- * 	3.2. 영역 내에 있다면? -> 겹치는 애들 있는지 봐야하는디? (아직 미정)
- * 		3.2.1. 겹치는 미생물 그룹 위치 확인
- * 		3.2.2. 수가 가장 많은 미생물 그룹에 나머지 미생물 그룹의 미생물 수 합산
- * 		3.2.3. 나머지 미생물은 groups에서 제거
- * 4. 남아있는 미생물 수 합산
+ * 	3.1. 미생물 그룹이 영역 마지막 위치가 아니라면 다음 반복 수행
+ * 	3.2. 미생물이 1개라면? 그룹 삭제 대상(deleteNumber)에 추가
+ * 	3.3. 그 이상이라면? 미생물 수 감소 및 방향 변경
+ * 4. 삭제 대상 그룹들 삭제
+ * 5. 남아있는 미생물 수 합산
  */
 public class ControlMicroganism {
 	static class MicroganismGroup {
@@ -66,15 +72,38 @@ public class ControlMicroganism {
 			initialize();
 			// logic
 			for (int time=1; time<=checkTime; time++) {
-				for (int number=0; number<groups.size(); number++) {
+				int[][] groupPosition = new int[bound][bound];
+				for (int index=0; index<bound; index++)
+					Arrays.fill(groupPosition[index], -1);
+				Stack<Integer> deleteNumber = new Stack<Integer>();
+				for (int number=0; number<groupCount; number++) {
 					MicroganismGroup group = groups.get(number);
 					group.move();
+					if (groupPosition[group.y][group.x] == -1)
+						groupPosition[group.y][group.x] = number;
+					else {
+						int existingNumber = groupPosition[group.y][group.x];
+						MicroganismGroup existingGroup = groups.get(existingNumber);
+						if (existingGroup.count > group.count) {
+							existingGroup.summision(group.count);
+							deleteNumber.push(number);
+						} else {
+							group.summision(existingGroup.count);
+							deleteNumber.push(existingNumber);
+							groupPosition[group.y][group.x] = number;
+						}
+					}
 				}
-				for (int number=0; number<groups.size(); number++) {
+				for (int number=0; number<groupCount; number++) {
 					MicroganismGroup group = groups.get(number);
-					if (inArea(group.x, group.y)) {
-						// 테두리 영역 안에 있는 경우 -> 동일 위치에 있는 그룹을 확인해야 함
-					} else group.die();
+					if (inArea(group.x, group.y)) continue;
+					if (group.count == 1) 
+						deleteNumber.push(number);
+					else group.die();
+				}
+				while (!deleteNumber.isEmpty()) {
+					groups.remove((int)deleteNumber.pop());
+					groupCount--;
 				}
 			}
 			for (MicroganismGroup group : groups)
