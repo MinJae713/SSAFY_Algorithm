4de6e5f9c2ac8485f8ea15b0c3350599c06510ad
YooMinJae <dbalsend@naver.com>
2025-09-15 23:32:05 +0900

    island end

--- src/homeworks/m9/d15/IslandBridge.java
+++ src/homeworks/m9/d15/IslandBridge.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -33,10 +35,10 @@ import java.util.StringTokenizer;
  * 4. bridgeCheckQueue에 요소가 있는 동안 반복
  * 	4.1. 큐에서 요소 poll (current)
  * 		4.1.1. 방향은 currentDelta로 지정
- * 	4.2. currentDelta 방향으로 다음 위치 값 확인,
+ * 	4.2. currentDelta 방향으로 다음 위치 값 확인 (next),
  * 		  다음 위치가 범위 내에 있고, 0인 동안 반복 (do-while)
- * 		4.2.1. 다음 위치가 0이라면 섬 간 거리 (distance) 1 증가
- * 	4.3. 다음 위치를 못찾았다면 continue
+ * 		4.2.1. 다음 위치가 범위 내에 있고, 0이라면 섬 간 거리 (distance) 1 증가
+ * 	4.3. next 위치가 범위를 벗어났다면 continue
  * 	4.4. distance가 1이면 continue
  * 	4.5. betweenDistance의 current 위치 번호, 찾은 섬 위치 번호 
  * 		  해당 값을 distance로 지정
@@ -45,28 +47,46 @@ import java.util.StringTokenizer;
  * 5. 각 섬간 가중치가 최소인 다리들 선택 (Prim)
  * 	5.1. 1번 섬 간선 가중치 (bridgeMinWeight) 0으로 지정
  * 	5.2. 1번 섬 가중치 우선순위 큐(islandWeightQueue)에 입력
+ * 		5.2.1. ToIslandBridge 클래스 인스턴스 생성 (island, distance)
  * 	5.3. 큐에 요소가 있는 동안 반복
- * 		5.3.1. 큐에서 요소 poll (current)
- * 		5.3.2. current 해당 bridgeMinWeight값을 
- * 			      총 다리 길이(totalDistance)에 합산
- * 		5.3.3. 방문 섬 개수(count) 1 증가
- * 		5.3.4. 모든 위치 섬들에 대해 반복 (nextIsland)
- * 			5.3.4.1. 해당 위치 섬간 거리가 0이거나, 초기값이면 continue
- * 			5.3.4.2. nextIsland 위치 bridgeMinWeight보다
- * 					 current-nextIsland 해당 가중치가 더 작다면?
- * 			5.3.4.3. nextIsland 위치 bridgeMinWeight를
- * 					 current-nextIsland 해당 가중치로 입력
- * 			5.3.4.4. islandWeightQueue에 nextIsland offer
+ * 		5.3.1. 큐에서 요소 poll (currentIsland)
+ * 		5.3.2. 처음 방문한 섬이라면? 
+ * 			5.3.2.1. currentIsland 해당 bridgeMinWeight값을 
+ * 			      	총 다리 길이(totalDistance)에 합산
+ * 			5.3.2.2. 섬 방문여부 true 지정
+ * 			5.3.2.3. 방문 섬 개수(visitedCount) 1 증가
+ * 		5.3.3. 모든 위치 섬들에 대해 반복 (nextIsland)
+ * 			5.3.3.1. 해당 위치 섬간 거리가 0이거나, 초기값이면 continue
+ * 			5.3.3.2. nextIsland 위치 bridgeMinWeight보다
+ * 					 currentIsland-nextIsland 해당 가중치가 더 작다면?
+ * 			5.3.3.3. nextIsland 위치 bridgeMinWeight를
+ * 					 currentIsland-nextIsland 해당 가중치로 입력
+ * 			5.3.3.4. islandWeightQueue에 nextIsland와 새로 지정한 
+ * 						bridgeMinWeight값을 파라미터로 ToIslandBridge 객체 생성 및 offer
  *
  */
 public class IslandBridge {
+	static class ToIslandBridge 
+			implements Comparable<ToIslandBridge>{
+		int island;
+		int distance;
+		public ToIslandBridge(int island, int distance) {
+			super();
+			this.island = island;
+			this.distance = distance;
+		}
+		@Override
+		public int compareTo(ToIslandBridge other) {
+			return Integer.compare(distance, other.distance);
+		}
+	}
 	private static BufferedReader reader;
 	private static StringTokenizer tokenizer;
 	private static int[][] delta;
 	private static int rowCount;
 	private static int columnCount;
 	private static int[][] map;
-	private static boolean[][] visited;
+	private static boolean[][] visitedPoint;
 	private static Queue<int[]> bridgeCheckQueue; 
 	// 각 섬들의 테두리 위치 및 확인 방향 입력
 	private static int islandCount;
@@ -75,28 +95,55 @@ public class IslandBridge {
 	// 섬간 거리 및 가중치 기록 - islandCount 결정 이후 초기화 됨
 	private static int totalDistance;
 	private static int visitedCount;
+	private static boolean[] visitedIsland;
 
 	public static void main(String[] args) throws IOException {
 		initialize();
 		// logic
-		for (int row=0; row<rowCount; row++) {
+		for (int row=0; row<rowCount; row++)
 			for (int column=0; column<columnCount; column++)  {
 				if (map[row][column] == 0 || 
-						visited[row][column]) continue;
+						visitedPoint[row][column]) continue;
 				checkSameIsland(column, row, ++islandCount);
 			}
-		}
-		forLog();
-		System.out.println(islandCount);
+		initializeBridge();
+		setMinimumBridgeDistance();
+		checkMinimumSpanningIslands(1);
 		
-		System.out.println(visitedCount == 0 ? -1 : totalDistance);
+		System.out.println(visitedCount == islandCount ? totalDistance : -1);
 		reader.close();
+	}
+
+	private static void checkMinimumSpanningIslands(int startIsland) {
+		bridgeMinWeight[startIsland] = 0;
+		Queue<ToIslandBridge> islandWeightQueue = new PriorityQueue<ToIslandBridge>();
+		islandWeightQueue.offer(new ToIslandBridge(startIsland, bridgeMinWeight[startIsland]));
+		while (!islandWeightQueue.isEmpty()) {
+			ToIslandBridge currentIsland = islandWeightQueue.poll();
+			if (!visitedIsland[currentIsland.island]) {
+				visitedIsland[currentIsland.island] = true;
+				totalDistance += currentIsland.distance;
+				visitedCount++;
+			}
+			for (int nextIsland=1; nextIsland<=islandCount; nextIsland++) {
+				if (betweenDistance[currentIsland.island][nextIsland] == 0 || 
+						betweenDistance[currentIsland.island][nextIsland] == Integer.MAX_VALUE) 
+					continue; // 인접이 아닌 경우
+				else if (bridgeMinWeight[nextIsland] > 
+						betweenDistance[currentIsland.island][nextIsland]) {
+					bridgeMinWeight[nextIsland] = 
+							betweenDistance[currentIsland.island][nextIsland];
+					islandWeightQueue.offer(new ToIslandBridge(
+							nextIsland, bridgeMinWeight[nextIsland]));
+				}
+			}
+		}
 	}
 
 	private static void checkSameIsland(int x, int y, int number) {
 		Queue<int[]> queue = new ArrayDeque<>();
 		queue.offer(new int[] {y, x});
-		visited[y][x] = true;
+		visitedPoint[y][x] = true;
 		map[y][x] = number;
 		while (!queue.isEmpty()) {
 			int[] current = queue.poll();
@@ -110,26 +157,56 @@ public class IslandBridge {
 							current[0], current[1], deltaIndex});
 				else {
 					// 같은 섬 영역
-					visited[nextY][nextX] = true;
+					visitedPoint[nextY][nextX] = true;
 					map[nextY][nextX] = number;
 					queue.offer(new int[] {nextY, nextX});
 				}
 			}
 		}
 	}
-
-	private static boolean checkNext(int nextY, int nextX) {
-		return 0<=nextY && nextY<rowCount && 
-				0<=nextX && nextX<columnCount &&
-				!visited[nextY][nextX];
+	
+	private static void initializeBridge() {
+		betweenDistance = new int[islandCount+1][islandCount+1];
+		for (int row=1; row<=islandCount; row++) {
+			Arrays.fill(betweenDistance[row], Integer.MAX_VALUE);
+			betweenDistance[row][row] = 0;
+		}
+		bridgeMinWeight = new int[islandCount+1];
+		Arrays.fill(bridgeMinWeight, Integer.MAX_VALUE);
+		visitedIsland = new boolean[islandCount+1];
+	}
+	
+	private static void setMinimumBridgeDistance() {
+		while (!bridgeCheckQueue.isEmpty()) {
+			int[] current = bridgeCheckQueue.poll();
+			int currentDelta = current[2];
+			int distance = 0;
+			int nextY = current[0];
+			int nextX = current[1];
+			do {
+				nextY += delta[currentDelta][0];
+				nextX += delta[currentDelta][1];
+				distance += inBound(nextY, nextX) && 
+						map[nextY][nextX] == 0 ? 1 : 0;
+			} while (inBound(nextY, nextX) && map[nextY][nextX] == 0);
+			if (!inBound(nextY, nextX) || distance == 1) continue;
+			// 여기는 섬을 찾은 경우
+			int fromIsland = map[current[0]][current[1]];
+			int toIsland = map[nextY][nextX];
+			distance = Math.min(distance, 
+					betweenDistance[fromIsland][toIsland]);
+			betweenDistance[fromIsland][toIsland] = distance;
+			betweenDistance[toIsland][fromIsland] = distance;
+		}
 	}
 
-	private static void forLog() {
-		for (int row=0; row<rowCount; row++) {
-			for (int column=0; column<columnCount; column++) 
-				System.out.printf("%-2d", map[row][column]);
-			System.out.println();
-		}
+	private static boolean checkNext(int nextY, int nextX) {
+		return inBound(nextY, nextX) && !visitedPoint[nextY][nextX];
+	}
+
+	private static boolean inBound(int nextY, int nextX) {
+		return 0<=nextY && nextY<rowCount && 
+				0<=nextX && nextX<columnCount;
 	}
 
 	private static void initialize() throws IOException {
@@ -139,7 +216,7 @@ public class IslandBridge {
 		rowCount = Integer.parseInt(tokenizer.nextToken());
 		columnCount = Integer.parseInt(tokenizer.nextToken());
 		map = new int[rowCount][columnCount];
-		visited = new boolean[rowCount][columnCount];
+		visitedPoint = new boolean[rowCount][columnCount];
 		bridgeCheckQueue = new ArrayDeque<int[]>();
 		islandCount = 0;
 		totalDistance = 0;
@@ -150,5 +227,4 @@ public class IslandBridge {
 				map[row][column] = Integer.parseInt(tokenizer.nextToken());
 		}
 	}
-
 }
